8cdf1e4508d5f81c798e00c24f3120a8f923db88
Brendan Eggers <eggers.brendan@gmail.com>
2021-05-18 21:24:51 -0400

    fix: leaving log in so I can debug properly

--- packages/cracl/lib/cracl.js
+++ packages/cracl/lib/cracl.js
@@ -5,4 +5,6 @@ exports.cracl = async () => {
   const args = process.argv;
   const userArgs = await parseOptions(args);
   const config = await generateConfig();
+
+  console.log(userArgs, config);
 };
