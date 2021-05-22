0e353802c13740364d6f73984a471d82133e517e
Brendan Eggers <eggers.brendan@gmail.com>
2021-05-22 15:10:49 -0400

    style: casing logic for file names

--- packages/cracl/lib/fileGenerator.js
+++ packages/cracl/lib/fileGenerator.js
@@ -1,17 +1,39 @@
+const {writeFileSync} = require('fs');
 const Handlebars = require('handlebars');
+const {pascalCase, camelCase} = require('change-case');
 
 const parseFileName = (type, name) => {
-  return name;
+  switch (type) {
+    case 'components':
+      // Parse to Pascal Case
+      return pascalCase(name);
+    case 'hooks':
+      const hasUse = name.slice(0, 3) === 'use';
+      if (hasUse) return camelCase(name);
+      else return `use${pascalCase(name)}`;
+    default:
+      console.error('Type not availble for generation');
+      process.exit(1);
+  }
 };
 
-const generateFiles = (path, type, name) => {
-  const finalPath = `${path}/${type}/${name}/${name}.js`;
+const writeToPath = async (filePath, js) => {
+  // Prompt to continue if file already exists
+  await writeFileSync(filePath, js);
+};
+
+const generateFiles = async (path, type, name) => {
+  // Grab the file template that needs to be used
   const templateString = require(`./templates/${type}.js`);
   const template = Handlebars.compile(templateString);
+  // Update the file name for consistency
   const filename = parseFileName(type, name);
 
-  // const context = {filename};
-  // const js = template(context);
+  // Generate the file and path with proper variables
+  const filePath = `${path}/${type}/${filename}/${filename}.js`;
+  const js = template({filename});
+
+  await writeToPath(filePath, js);
 };
 
 module.exports = {
